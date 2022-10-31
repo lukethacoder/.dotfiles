cf9d5437143ffe0d142aad7b1b8f624f5c53524b
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2022-10-31 20:28:36 +0100

    migrate to oh-my-posh from starship.rs

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,61 @@
-# In *Windows PowerShell*, prepend `$OutputEncoding = `
-# $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+# Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
+function dirs {
+  if ($args.Count -gt 0) {
+    Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
+  }
+  else {
+    Get-ChildItem -Recurse | Foreach-Object FullName
+  }
+}
 
-Invoke-Expression Clear-Host
-Invoke-Expression (&starship init powershell)
+function g { cd C:\Users\lukes\Github }
+
+function get-ip {
+  (Invoke-WebRequest http://ifconfig.me/ip ).Content
+}
+
+function uptime {
+  Get-WmiObject win32_operatingsystem | select csname, @{LABEL='LastBootUpTime';
+  EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
+}
+
+function reload-profile {
+  & $profile
+}
+
+function find-file($name) {
+  ls -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | foreach {
+    $place_path = $_.directory
+    echo "${place_path}\${_}"
+  }
+}
+
+function grep($regex, $dir) {
+  if ( $dir ) {
+    ls $dir | select-string $regex
+    return
+  }
+  $input | select-string $regex
+}
+
+function touch($file) {
+  "" | Out-File $file -Encoding ASCII
+}
+
+function sed($file, $find, $replace){
+  (Get-Content $file).replace("$find", $replace) | Set-Content $file
+}
+function which($name) {
+  Get-Command $name | Select-Object -ExpandProperty Definition
+}
+function export($name, $value) {
+  set-item -force -path "env:$name" -value $value;
+}
+function pkill($name) {
+  ps $name -ErrorAction SilentlyContinue | kill
+}
+function pgrep($name) {
+  ps $name
+}
+
+oh-my-posh init pwsh | Invoke-Expression
